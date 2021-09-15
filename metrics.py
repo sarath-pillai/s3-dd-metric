96ad72000b4f1ea9e28f286e2380dc3caf3a3455
Sarath Pillai <sarath@slashroot.in>
2021-09-15 11:58:31 +0530

    Adding initial commit with transaction list feature for today and yesterday

---
+++ metrics.py
@@ -0,0 +1,37 @@
+import boto3
+import fire
+import datetime
+import json
+
+s3 = boto3.client('s3')
+
+
+def get_transaction_ids(bucket_name, today):
+    yesterday = datetime.date.fromisoformat(today)-datetime.timedelta(1)
+    yesterday = yesterday.strftime('%Y-%m-%d')
+    transactions = {}
+    todays_files = s3.list_objects(Bucket = bucket_name, Prefix=today)
+    todays_transactions = []
+    if todays_files.get('Contents') != None:
+        for file in todays_files.get('Contents'):
+            content = s3.get_object(Bucket=bucket_name, Key=file.get('Key'))
+            json_data = json.loads(content['Body'].read().decode("utf-8"))
+            transaction_id = json_data['transaction_id'] 
+            todays_transactions.append(transaction_id)
+    yesterdays_files = s3.list_objects(Bucket = bucket_name, Prefix=yesterday)
+    yesterdays_transactions = []
+    if yesterdays_files.get('Contents') != None:
+        for file in yesterdays_files.get('Contents'):
+            content = s3.get_object(Bucket=bucket_name, Key=file.get('Key'))
+            json_data = json.loads(content['Body'].read().decode("utf-8"))
+            transaction_id = json_data['transaction_id']
+            yesterdays_transactions.append(transaction_id)
+    return {'yesterdays_transactions': yesterdays_transactions, 'todays_transactions': yesterdays_transactions}
+
+
+def main(bucketName, date):
+    print(get_transaction_ids(bucketName, date))
+
+
+if __name__ == '__main__':
+    fire.Fire(main)
