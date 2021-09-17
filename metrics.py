04ee503f2c72fae5df841651db5180e9d61ed368
Sarath Pillai <sarath@slashroot.in>
2021-09-17 15:19:58 +0530

    Making some more tweaks to send to dd as well as docstrings

--- metrics.py
+++ metrics.py
@@ -1,12 +1,20 @@
 import boto3
 import fire
+import os
 import datetime
 import json
+from datadog import initialize, api
+
 
 s3 = boto3.client('s3')
 
 
 def get_transaction_ids(bucket_name, today):
+    """
+    This function gets all objects from the required dates and then gets transactionIds from each of those.
+    The function then returns just the list of transactions found with their values. It does not return the count or any metrics. 
+    This way its easier to view the transaction Ids as well if required. 
+    """
     try:
         yesterday = datetime.date.fromisoformat(today)-datetime.timedelta(1)
         yesterday = yesterday.strftime('%Y-%m-%d')
@@ -32,6 +40,10 @@ def get_transaction_ids(bucket_name, today):
 
 
 def transaction_metrics(bucket_name, today):
+    """
+    This function does the actual math and calculates if there any new transaction ids or not, and if there is how many of them are there. 
+    It also returns the lost transactions.
+    """
     try:
         transaction_data = get_transaction_ids(bucket_name, today)
         yesterday = transaction_data['yesterdays_transactions']
@@ -45,14 +57,41 @@ def transaction_metrics(bucket_name, today):
     except Exception as e:
         print ("Error happened while Generating Metrics: "+e)
         
-def main(bucketName, date=datetime.datetime.today().strftime('%Y-%m-%d'), target="Print"):
+
+def send_to_datadog(metrics):
+    """
+    This function does the job of send a given set of metrics to datadog. 
+    The function accept a list of maps containing the metrics.
+    """
+    try:
+        if "DATADOG_API_KEY" in os.environ:
+            initialize()
+            for m in metrics:
+                host = None
+                api.Metric.send(
+                    metric=m['metric'],
+                    points=m['value'],
+                    tags=[],
+                    host=host
+                )
+    except Exception as e:
+        print ("Error happened while Sending Data to DataDog: "+e) 
+
+def main(bucketName, date=datetime.datetime.today().strftime('%Y-%m-%d'), target="Metric"):
+    """
+    This is a simple Python script that can grab objects from a given S3 bucket and optionally send metrics related to transactions to datahub.
+    """
     try:
         if target == 'Print':
             print(get_transaction_ids(bucketName, date))
         if target == 'Datadog':
-            pass
-        if target == 'Metric':
+            metrics = []
             new, lost = transaction_metrics(bucketName, date)
+            metrics.append(new)
+            metrics.append(lost)
+            send_to_datadog(metrics)
+        if target == 'Metric':
+            new, lost  = transaction_metrics(bucketName, date)
             print (new, lost, sep='\n')
     except Exception as e:
         print ("Something Went Wrong: "+e)
