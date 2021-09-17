740c5c3030c69b490b0b0e7a3c184560a4002f87
Sarath Pillai <sarath@slashroot.in>
2021-09-17 14:19:40 +0530

    Adding metric logic

--- metrics.py
+++ metrics.py
@@ -10,7 +10,6 @@ def get_transaction_ids(bucket_name, today):
     try:
         yesterday = datetime.date.fromisoformat(today)-datetime.timedelta(1)
         yesterday = yesterday.strftime('%Y-%m-%d')
-        transactions = {}
         todays_files = s3.list_objects(Bucket = bucket_name, Prefix=today)
         todays_transactions = []
         if todays_files.get('Contents') != None:
@@ -29,15 +28,32 @@ def get_transaction_ids(bucket_name, today):
                 yesterdays_transactions.append(transaction_id)
     except Exception as e:
         print ("Error happened while Fetching TansactionIds: "+e)
-    return {'yesterdays_transactions': yesterdays_transactions, 'todays_transactions': yesterdays_transactions}
+    return {'yesterdays_transactions': yesterdays_transactions, 'todays_transactions': todays_transactions}
 
 
+def transaction_metrics(bucket_name, today):
+    try:
+        transaction_data = get_transaction_ids(bucket_name, today)
+        yesterday = transaction_data['yesterdays_transactions']
+        today = transaction_data['todays_transactions']
+        t = round(datetime.datetime.now().timestamp())
+        new_transactions = len(list(set(today) - set(yesterday)))
+        lost_transactions = len(list(set(yesterday) - set(today)))
+        new_transactions_metrics = { "metric": "business.a_process.transaction_new", "value": new_transactions, "timestamp": t }
+        lost_transactions_metrics = { "metric": "business.a_process.transaction_lost", "value": lost_transactions, "timestamp": t }
+        return (new_transactions_metrics, lost_transactions_metrics)
+    except Exception as e:
+        print ("Error happened while Generating Metrics: "+e)
+        
 def main(bucketName, date=datetime.datetime.today().strftime('%Y-%m-%d'), target="Print"):
     try:
         if target == 'Print':
             print(get_transaction_ids(bucketName, date))
         if target == 'Datadog':
             pass
+        if target == 'Metric':
+            new, lost = transaction_metrics(bucketName, date)
+            print (new, lost, sep='\n')
     except Exception as e:
         print ("Something Went Wrong: "+e)
 
